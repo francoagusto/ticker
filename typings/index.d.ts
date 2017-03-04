5b2ddaa00f69fdcb5cf7222ebd958dfdb1f230fa
Franco Barletta <franco.barletta@dubitlimited.com>
2017-03-04 11:30:26 -0300

    remove files

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/ifvisible/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/js-signals/index.d.ts" />
-/// <reference path="modules/typescript-collections/index.d.ts" />
